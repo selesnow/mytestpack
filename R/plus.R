4fb86e0d86803c62e77d1759419fac80335021dc
Alexey <selesnow@gmail.com>
2023-08-03 18:00:04 +0300

    doc

--- R/plus.R
+++ R/plus.R
@@ -1,1 +1,13 @@
-plus <- function(x, y) return(x + y)
+#' Sum of two numbers
+#'
+#' @param x first number
+#' @param y second number
+#'
+#' @return sum of x and y
+#' @export
+#'
+#' @examples
+#' plus(4, 6)
+plus <- function(x, y) {
+  return(x + y)
+}
